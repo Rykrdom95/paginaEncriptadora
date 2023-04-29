60bc1753f87c4a51171d12ba153356cbba8da85d
Ricardo <rychy95@gmail.com>
2023-04-29 14:30:26 -0500

    1.0

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
-function execute() {
-    const input = document.getElementById("input").value.toLowerCase();
-    const option = document.getElementById("option").value;
-    let output = "";
-    if (option === "encrypt") {
-      output = encrypt(input);
-    } else {
-      output = decrypt(input);
+function ejecutar() {
+  var texto = document.getElementById("texto").value;
+  var opciones = document.getElementById("opciones").value;
+  var resultado = "";
+
+  if (opciones === "encriptar") {
+    resultado = encriptar(texto);
+  } else {
+    resultado = desencriptar(texto);
+  }
+
+  document.getElementById("resultado").value = resultado;
+}
+
+function encriptar(texto) {
+  var resultado = "";
+  for (var i = 0; i < texto.length; i++) {
+    var letra = texto.charAt(i);
+    switch (letra) {
+      case "e":
+        resultado += "enter";
+        break;
+      case "i":
+        resultado += "imes";
+        break;
+      case "a":
+        resultado += "ai";
+        break;
+      case "o":
+        resultado += "ober";
+        break;
+      case "u":
+        resultado += "ufat";
+        break;
+      default:
+        resultado += letra;
     }
-    document.getElementById("output").value = output;
   }
-  
-  function encrypt(word) {
-    word = word.replace(/e/g, "enter");
-    word = word.replace(/i/g, "imes");
-    word = word.replace(/a/g, "ai");
-    word = word.replace(/o/g, "ober");
-    word = word.replace(/u/g, "ufat");
-    return word;
+  return resultado;
+}
+
+function desencriptar(texto) {
+  var resultado = "";
+  for (var i = 0; i < texto.length; i++) {
+    var letra = texto.substring(i, i + 5);
+    if (letra === "enter") {
+      resultado += "e";
+    } else if (letra === "imes ") {
+      resultado += "i";
+    } else if (letra === "ai") {
+      resultado += "a";
+    } else if (letra === "ober") {
+      resultado += "o";
+    } else if (letra === "ufat") {
+      resultado += "u";
+    } else {
+      resultado += letra.substring(0, 1);
+    }
+    i += 4;
   }
-  
-  function decrypt(word) {
-    word = word.replace(/enter/g, "e");
-    word = word.replace(/imes/g, "i");
-    word = word.replace(/ai/g, "a");
-    word = word.replace(/ober/g, "o");
-    word = word.replace(/ufat/g, "u");
-    return word;
-  }
-  
-  function copy() {
-    const output = document.getElementById("output");
-    output.select();
-    output.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-  }
-  
+  return resultado;
+}
+
+function copiar() {
+  var resultado = document.getElementById("resultado");
+  resultado.select();
+  resultado.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("El texto se ha copiado al portapapeles.");
+}
